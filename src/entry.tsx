00024964648827000a4bda49788350d529cbb225
ryland goldstein <rylandgoldstein@rylands-MacBook-Pro.local>
2025-08-28 11:45:00 -0700

    refactor name of exported files

--- src/entry.tsx
+++ src/entry.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router';
 import Application from './application';
-import './index.css';
+import './entry.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
